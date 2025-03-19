8528a6cf7aaa96b2e2bbb6fcf043cab0ad8c51a5
ghiles <ghiles>
2025-03-19 13:20:23 +0100

    commit sur branche vide pour les frontend pusher ici

--- jsk/match_future/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MatchFutureConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'match_future'
--- jsk/stadium_info/serializers.py
+++
@@ -1,8 +0,0 @@
-# stadium_info/serializers.py
-from rest_framework import serializers
-from .models import StadiumInfo
-
-class StadiumInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StadiumInfo
-        fields = '__all__'
